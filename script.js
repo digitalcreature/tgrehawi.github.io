32c51023227f76e1d86b57df5c112f64a950aec7
Tanner Grehawick <tgrehawi@gmail.com>
2016-10-09 21:42:38 -0400

    modal now is close by escape

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+activemodal = null;
 
 function showmodal(modalid) {
-	modal = document.getElementById(modalid);
-	modal.style.display = "block";
-	return false;
+	if (activemodal) activemodal.style.display = "none";
+	activemodal = document.getElementById(modalid);
+	activemodal.style.display = "block";
 }
-
-
 
 listeners = {
 	hide: function() {
 		this.style.display = "none";
+		if (activemodal == this) activemodal = null;
 	},
 	absorb: function(event) {
 		event.stopPropagation();
 	},
 };
 
+document.onkeydown = function (event) {
+	if (event.keyCode == 27) {	//escape
+		if (activemodal) {
+			activemodal.hide();
+		}
+	}
+}
+
 modals = document.getElementsByClassName("modal");
 for (modal of modals) {
+	modal.hide = listeners.hide;
 	modal.onclick = listeners.hide;
 	content = modal.getElementsByClassName("modal-content");
 	content = content[0];
